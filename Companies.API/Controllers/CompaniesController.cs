44d79301e1c2a640bcd2d8f866f047138c845832
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-11-26 14:15:56 +0100

    SeedData

--- Companies.API/Controllers/CompaniesController.cs
+++ Companies.API/Controllers/CompaniesController.cs
@@ -25,7 +25,7 @@ namespace Companies.API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Company>>> GetCompany()
         {
-            return await _context.Company.ToListAsync();
+            return await _context.Company./*Include(c => c.Employees)*/ToListAsync();
         }
 
         // GET: api/Companies/5
@@ -44,65 +44,65 @@ namespace Companies.API.Controllers
 
         // PUT: api/Companies/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutCompany(int id, Company company)
-        {
-            if (id != company.Id)
-            {
-                return BadRequest();
-            }
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutCompany(int id, Company company)
+        //{
+        //    if (id != company.Id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(company).State = EntityState.Modified;
+        //    _context.Entry(company).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CompanyExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!CompanyExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        // POST: api/Companies
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Company>> PostCompany(Company company)
-        {
-            _context.Company.Add(company);
-            await _context.SaveChangesAsync();
+        //// POST: api/Companies
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<Company>> PostCompany(Company company)
+        //{
+        //    _context.Company.Add(company);
+        //    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetCompany", new { id = company.Id }, company);
-        }
+        //    return CreatedAtAction("GetCompany", new { id = company.Id }, company);
+        //}
 
-        // DELETE: api/Companies/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCompany(int id)
-        {
-            var company = await _context.Company.FindAsync(id);
-            if (company == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/Companies/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteCompany(int id)
+        //{
+        //    var company = await _context.Company.FindAsync(id);
+        //    if (company == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            _context.Company.Remove(company);
-            await _context.SaveChangesAsync();
+        //    _context.Company.Remove(company);
+        //    await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        private bool CompanyExists(int id)
-        {
-            return _context.Company.Any(e => e.Id == id);
-        }
+        //private bool CompanyExists(int id)
+        //{
+        //    return _context.Company.Any(e => e.Id == id);
+        //}
     }
 }
--- Companies.API/Program.cs
+++ Companies.API/Program.cs
@@ -6,13 +6,13 @@ namespace Companies.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<CompaniesContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("CompaniesContext") ?? throw new InvalidOperationException("Connection string 'CompaniesContext' not found.")));
 
             // Add services to the container.
+            builder.Services.AddDbContext<CompaniesContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("CompaniesContext") ?? throw new InvalidOperationException("Connection string 'CompaniesContext' not found.")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,6 +26,7 @@ namespace Companies.API
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
+                await app.SeedDataAsync();
             }
 
             app.UseHttpsRedirection();
