d3c50cbd361e35969030f335e6797d1fe21776e0
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-12-03 15:12:52 +0100

    Start on EmployeeRepository

---
+++ Companies.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using Companies.Infrastructure.Data;
+using Domain.Contracts;
+using Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Companies.Infrastructure.Repositories;
+public class EmployeeRepository : RepositoryBase<Employee>
+{
+    public EmployeeRepository(CompaniesContext context) :base(context){}
+
+}
--- Domain.Contracts/ICompanyRepository.cs
+++ Domain.Contracts/ICompanyRepository.cs
@@ -5,6 +5,7 @@ using Domain.Models.Entities;
 namespace Domain.Contracts;
 public interface ICompanyRepository
 {
+    void Update(Company company);
     void Create(Company company);
     void Delete(Company company);
     Task<IEnumerable<Company>> GetCompaniesAsync(bool includeEmployees = false, bool trackChanges = false);
