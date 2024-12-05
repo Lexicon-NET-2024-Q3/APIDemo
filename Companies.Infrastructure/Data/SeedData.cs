5ed1dcd22099e35e09642596b37ae44fae06e968
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-12-05 13:44:50 +0100

    Seed works!

--- Companies.Infrastructure/Data/SeedData.cs
+++ Companies.Infrastructure/Data/SeedData.cs
@@ -1,27 +1,45 @@
 ﻿using Bogus;
 using Domain.Models.Entities;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Companies.Infrastructure.Data;
 
 public static class SeedData
 {
+    private static UserManager<ApplicationUser> userManager = null!;
+    private static RoleManager<IdentityRole> roleManager = null!;
+    private static IConfiguration configuration = null!;
+    private const string employeeRole = "Employee";
+    private const string adminRole = "Admin";
+
     public static async Task SeedDataAsync(this IApplicationBuilder builder)
     {
         using (var scope = builder.ApplicationServices.CreateScope())
         {
-            var serviceprovider = scope.ServiceProvider;
-            var db = serviceprovider.GetRequiredService<CompaniesContext>();
+            var serviceProvider = scope.ServiceProvider;
+            var db = serviceProvider.GetRequiredService<CompaniesContext>();
 
             await db.Database.MigrateAsync();
             if (await db.Companies.AnyAsync()) return;
 
+            userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>() ?? throw new ArgumentNullException(nameof(userManager));
+            roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+            //ToDo: Null check 
+
+
             try
             {
+                await CreateRolesAsync(new[] { adminRole, employeeRole });
                 var companies = GenerateCompanies(4);
                 db.AddRange(companies);
+                await GenerateEmployeesAsync(30, companies);
                 await db.SaveChangesAsync();
             }
             catch (Exception ex)
@@ -32,20 +50,32 @@ public static class SeedData
         }
     }
 
-    private static IEnumerable<Company> GenerateCompanies(int nrOfCompanies)
+    private static async Task CreateRolesAsync(string[] roleNames)
+    {
+        foreach (var roleName in roleNames)
+        {
+            if (await roleManager.RoleExistsAsync(roleName)) continue;
+            var role = new IdentityRole { Name = roleName };
+            var result = await roleManager.CreateAsync(role);
+
+            if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
+        }
+    }
+
+    private static List<Company> GenerateCompanies(int nrOfCompanies)
     {
         var faker = new Faker<Company>("sv").Rules((f, c) =>
         {
             c.Name = f.Company.CompanyName();
             c.Address = $"{f.Address.StreetAddress()}, {f.Address.City()}";
             c.Country = f.Address.Country();
-            c.Employees = GenerateEmployees(f.Random.Int(min: 2, max: 10));
+           // c.Employees = GenerateEmployees(f.Random.Int(min: 2, max: 10));
         });
 
         return faker.Generate(nrOfCompanies);
     }
 
-    private static ICollection<ApplicationUser> GenerateEmployees(int nrOfEmplyees)
+    private static async Task GenerateEmployeesAsync(int nrOfEmplyees, List<Company> companies)
     {
         string[] positions = ["Developer", "Tester", "Manager"];
 
@@ -54,9 +84,31 @@ public static class SeedData
             e.Name = f.Person.FullName;
             e.Age = f.Random.Int(min: 18, max: 70);
             e.Position = positions[f.Random.Int(0, positions.Length - 1)];
+            e.Email = f.Person.Email;
+            e.UserName = f.Person.UserName;
+            e.Company = companies[f.Random.Int(0, companies.Count -1)];
         });
 
-        return faker.Generate(nrOfEmplyees);
+        var users =  faker.Generate(nrOfEmplyees);
+
+        var passWord = configuration["password"];
+        if (string.IsNullOrEmpty(passWord))
+            throw new Exception("password nor found");
+
+        foreach (var user in users)
+        {
+            var result = await userManager.CreateAsync(user, passWord);
+            if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
+
+            if (user.Position == "Manager")
+            {
+                await userManager.AddToRoleAsync(user, adminRole);
+            }
+            else
+            {
+                await userManager.AddToRoleAsync(user, employeeRole);
+            }
+        }
     }
 
 
