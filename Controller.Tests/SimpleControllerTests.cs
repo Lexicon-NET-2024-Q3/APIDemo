c951c9f3f24b3a1967527537eb7147da11f6dfa5
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-12-13 14:13:46 +0100

    Mocked ClaimsPrincipal

--- Controller.Tests/SimpleControllerTests.cs
+++ Controller.Tests/SimpleControllerTests.cs
@@ -2,6 +2,7 @@ using Companies.Presemtation.Controllers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System.Security.Claims;
 
 namespace Controller.Tests;
 
@@ -41,4 +42,29 @@ public class SimpleControllerTests
         Assert.IsType<BadRequestObjectResult>(resultType);
         Assert.Equal("is not auth", resultType.Value);
     }
+
+    [Fact]
+    public async Task GetCompany_IfNotAuth_ShouldReturn400BadRequest2() 
+    {
+        var mockClaimsPrincipal = new Mock<ClaimsPrincipal>();
+        mockClaimsPrincipal.SetupGet(x => x.Identity.IsAuthenticated).Returns(false);
+
+        var sut = new SimpleController();
+        sut.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+            {
+                User = mockClaimsPrincipal.Object
+            }
+        };
+
+        var result = await sut.GetCompany();
+        var resultType = result.Result as BadRequestObjectResult;
+
+        Assert.IsType<BadRequestObjectResult>(resultType);
+        Assert.Equal(StatusCodes.Status400BadRequest, resultType.StatusCode);
+
+
+    }
+
 }
