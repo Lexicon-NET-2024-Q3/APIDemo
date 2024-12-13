38cd4e98b73c661b09d70bdc0960e8f892683485
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-12-13 14:42:03 +0100

    RepositoryController

---
+++ Companies.Presemtation/ControllersForTestDemo/RepositoryController.cs
@@ -0,0 +1,32 @@
+﻿using Companies.API.DTOs;
+using Companies.Shared.Request;
+using Domain.Contracts;
+using Domain.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+using System.Text.Json;
+
+namespace Companies.Presemtation.ControllersForTestDemo;
+
+[Route("api/repo/{id}")]
+[ApiController]
+public class RepositoryController : ControllerBase
+{
+    private readonly IEmployeeRepository employeeRepo;
+
+    public RepositoryController(IEmployeeRepository employeeRepo)
+    {
+        this.employeeRepo = employeeRepo;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetEmployees(int id)
+    {
+        var employees = await employeeRepo.GetEmployeesAsync(id);
+
+        return Ok(employees);
+    }
+}
---
+++ Controller.Tests/RepoControollerTest.cs
@@ -0,0 +1,65 @@
+﻿using Companies.Presemtation.ControllersForTestDemo;
+using Domain.Contracts;
+using Domain.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller.Tests;
+public class RepoControollerTest
+{
+    private Mock<IEmployeeRepository> mockRepo;
+    private RepositoryController sut;
+
+    public RepoControollerTest()
+    {
+         mockRepo = new Mock<IEmployeeRepository>();
+         sut = new RepositoryController(mockRepo.Object);
+
+    }
+
+    [Fact]
+    public async Task GetEmployees_ShouldReturnAllEmplyees()
+    {
+        var users = GetUsers();
+        mockRepo.Setup(x => x.GetEmployeesAsync(It.IsIn<int>(2,3), It.IsAny<bool>())).ReturnsAsync(users);
+
+        var result = await sut.GetEmployees(2);
+        var resultType = result.Result as OkObjectResult;
+
+        //Assert
+        Assert.IsType<OkObjectResult>(resultType);
+        Assert.Equal(StatusCodes.Status200OK, resultType.StatusCode);
+        var items = resultType.Value as List<ApplicationUser>;
+        Assert.IsType<List<ApplicationUser>>(items);
+        Assert.Equal(items.Count(), users.Count);
+
+    }
+
+    private List<ApplicationUser> GetUsers()
+    {
+        return new List<ApplicationUser>
+            {
+                new ApplicationUser
+                {
+                     Id = "1",
+                     Name = "Kalle",
+                     Age = 12,
+                     UserName = "Kalle"
+                },
+               new ApplicationUser
+                {
+                     Id = "2",
+                     Name = "Kalle",
+                     Age = 12,
+                     UserName = "Kalle"
+                },
+            };
+
+    }
+}
